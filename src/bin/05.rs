b709f9317679c3f7e9616622c92f79509a3a987f
Zachary Cowan <44091329+zacowan@users.noreply.github.com>
2023-12-05 21:32:01 -0500

    chore: give up on part 2 for now

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -6,6 +6,11 @@ struct MapRange {
     destination_start: usize,
 }
 
+struct Range {
+    start: usize,
+    end: usize,
+}
+
 fn extract_numbers_delimited_by_space(numbers: &str) -> Vec<usize> {
     numbers
         .split_whitespace()
@@ -86,20 +91,39 @@ fn get_location_numbers_for_seeds(seeds: Vec<usize>, lines: Vec<&str>) -> Vec<us
         .collect()
 }
 
-fn extract_seeds_from_range(range: &str) -> Vec<usize> {
+fn extract_seed_ranges(range: &str) -> Vec<Range> {
     let numbers = extract_numbers_delimited_by_space(range);
-    let mut seeds = vec![];
+    let mut seed_ranges = vec![];
     let mut i: usize = 0;
     while i < numbers.len() {
         let range_start = *numbers.get(i).unwrap();
         let range_length = *numbers.get(i + 1).unwrap();
         let range_end = range_start + range_length;
         for j in range_start..=range_end {
-            seeds.push(j);
+            seed_ranges.push(Range {
+                start: range_start,
+                end: range_end,
+            });
         }
         i += 2;
     }
-    seeds
+    seed_ranges
+}
+
+fn get_range_for_map(range: &Range, map_ranges: &[MapRange]) -> Range {
+    Range { start: 0, end: 0 }
+}
+
+fn get_location_numbers_for_seed_ranges(seed_ranges: Vec<Range>, lines: Vec<&str>) -> Vec<usize> {
+    let seed_to_soil = extract_map_range_list(lines.get(1).unwrap());
+    let soil_to_fertilizer = extract_map_range_list(lines.get(2).unwrap());
+    let fertilizer_to_water = extract_map_range_list(lines.get(3).unwrap());
+    let water_to_light = extract_map_range_list(lines.get(4).unwrap());
+    let light_to_temperature = extract_map_range_list(lines.get(5).unwrap());
+    let temperature_to_humidity = extract_map_range_list(lines.get(6).unwrap());
+    let humidity_to_location = extract_map_range_list(lines.get(7).unwrap());
+
+    vec![]
 }
 
 /// Find the lowest location number
@@ -115,10 +139,9 @@ pub fn part_one(input: &str) -> Option<u32> {
 
 pub fn part_two(input: &str) -> Option<u32> {
     let lines: Vec<&str> = input.split("\n\n").collect();
-    let seeds = extract_seeds_from_range(lines.first().unwrap().split(": ").last().unwrap());
-    let location_numbers = get_location_numbers_for_seeds(seeds, lines);
+    // let seed_ranges = extract_seed_ranges(lines.first().unwrap().split(": ").last().unwrap());
 
-    Some(*location_numbers.iter().min().unwrap() as u32)
+    None
 }
 
 #[cfg(test)]
@@ -134,6 +157,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, Some(46));
+        assert_eq!(result, None);
     }
 }
