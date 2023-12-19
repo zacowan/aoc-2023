3ffaf50440cab9d4e076868822d6a6696b3e4fc5
Zachary Cowan <44091329+zacowan@users.noreply.github.com>
2023-12-18 22:15:50 -0500

    fix: read a valid input file

--- src/bin/10.rs
+++ src/bin/10.rs
@@ -102,7 +102,9 @@ mod tests {
 
     #[test]
     fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        let result = part_two(&advent_of_code::template::read_file_part(
+            "examples", DAY, 2,
+        ));
         assert_eq!(result, None);
     }
 }
